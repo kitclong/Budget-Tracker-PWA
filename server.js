5de6862094be153a56387231899f222aa7572e41
kitclong <78049224+kitclong@users.noreply.github.com>
2021-07-23 02:06:09 -0400

    update code

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(
